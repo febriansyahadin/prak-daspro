87c652b8d61e312623f8c0a2532125dcde440122
febriansyahadin <febriansyahhoho@gmail.com>
2023-09-26 20:22:21 +0700

    modifikasi

--- PemilihanPercobaan211.java
+++ PemilihanPercobaan211.java
@@ -15,6 +15,21 @@ public class PemilihanPercobaan211{
         String message = total < 65 ? "Remidi" : "Tidak remidi"; 
         System.out.println("Nilai akhir = " + total + " sehingga " + message);
 
+        if (total >80 &&total<=100) {
+            System.out.println("Nilai huruf = A");
+        } else if (total >73 && total<= 80) {
+            System.out.println("Nilai huruf = B+");
+        } else if (total >65 && total<= 73) {
+            System.out.println("Nilai huruf B");
+        } else if (total >60 && total<=65) {
+            System.out.println("Nilai huruf C+");
+        } else if (total >50 && total<=60) {
+            System.out.println("Nilai huruf C");
+        } else if (total >39 && total<=50) {
+            System.out.println("Nilai huruf D");  
+        } else if (total <=39) {
+            System.out.println("Nilai huruf E");
+        }
 
     }
 
