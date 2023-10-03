a32cfff29e6e5a33ab5d203932a7b40de79b5de9
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-03 20:45:19 +0700

    commit

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan111 {
+public class Pemilihan2Percobaan111{
     public static void main(String[] args) {
         Scanner input11 = new Scanner(System.in);
-        System.out.print("Masukkan tahun:");
-        int tahun = input11.nextInt();
+        int tahun;
+        System.out.println("Masukkan tahun:");
+        tahun = input11.nextInt();
     if ((tahun % 4) == 0) {
         System.out.println("Tahun Kabisat");
     } else
