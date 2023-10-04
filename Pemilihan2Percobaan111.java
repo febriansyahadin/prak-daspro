98bb3e6bfe7ad7a5f53f351546e84a7412d976d9
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-04 10:10:15 +0700

    commit

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -8,13 +8,18 @@ public class Pemilihan2Percobaan111{
         tahun = input11.nextInt();
     if ((tahun % 4) == 0 ) {
         if (tahun % 100== 0) {
-        if (tahun % 400== 0)  {  
-        }       
-        }  
-            System.out.println("Tahun kabisat");       
+        if (tahun % 400== 0)  {
+            System.out.println("Tahun kabisat");
         } else {
+            System.out.println("Bukan Tahun kabisat");
+        }         
+        } else {
+            System.out.println("Tahun kabisat");
+
+        }    
+        } else 
              System.out.println("Bukan tahun kabisat");
-        }
+        
     } 
 
     } 
