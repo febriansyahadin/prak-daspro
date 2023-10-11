3b5a45419f6a4853f9c832f29b18aa186dd606bf
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-11 11:22:44 +0700

    commit

--- ForKelipatan11.java
+++ ForKelipatan11.java
@@ -5,6 +5,7 @@ public class ForKelipatan11 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, total = 0, counter = 0;
+        
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
 
@@ -12,11 +13,13 @@ public class ForKelipatan11 {
             if (i % kelipatan == 0) {
                 total += i;
                 counter++;
+                
             }
         }
-
+        double rataRata = (double) total / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("rataRata bilangan kelipatan %d dari  1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
 
     }
 }
