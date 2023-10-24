e409d8ac842de9b1cc79aa1ece554b9fb674bd9d
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-24 21:38:50 +0700

    p

--- Prak-jobsheet 3/ContohVariabel12.java
+++ Prak-jobsheet 3/ContohVariabel12.java
@@ -7,12 +7,12 @@ public class ContohVariabel12 {
         String salahSatuHobySayaAdalah = "Bermain sepak bola";
         boolean isPandai = true;
         char jenisKelamin = 'L';
-        byte _umurSayaSekarang = 19;
+        byte umurSayaSekarang = 19;
         double $ipk = 3.24, tinggi = 1.78;
         System.out.println("salahSatuHobySayaAdalah " + salahSatuHobySayaAdalah);
         System.out.println("Apakah pandai? " + isPandai);
         System.out.println("JenisKelamin: " + jenisKelamin);
-        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println("Umurku saat ini: " + umurSayaSekarang);
         System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
     }
 }
