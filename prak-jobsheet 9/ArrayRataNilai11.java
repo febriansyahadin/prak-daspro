1740d28135a70f287bbbf2b18407c71fed8e6733
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-25 08:03:02 +0700

    commit

--- prak-jobsheet 9/ArrayRataNilai11.java
+++ prak-jobsheet 9/ArrayRataNilai11.java
@@ -7,17 +7,19 @@ import java.util.Scanner;
             int[] nilaiMhs= new int[10];
             double total = 0;
             double rata2;
+            int jumlahLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1+" : "));
+            System.out.print("Masukkan nilai mahasiswa ke-"+i+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+        
         }
         
             rata2 = total/nilaiMhs.length;
             System.out.println("Rata-rata nilai = "+rata2);
-
+            
         }
     }
