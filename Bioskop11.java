ecc402feba13c5955732244038ed71b4509e147c
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-30 19:42:01 +0700

    commit

--- Bioskop11.java
+++ Bioskop11.java
@@ -23,6 +23,10 @@ public class Bioskop11 {
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang baris ke-" + (i+i) + " : " + penonton[i].length);
         }
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i =0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
 
     }
 }
