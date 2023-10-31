9d2bf6a7e517345b3d863edad9a25d1a637e4784
febriansyahadin <febriansyahhoho@gmail.com>
2023-10-31 16:54:28 +0700

    commit pertayaan 5

--- jobsheet10/Bioskop11.java
+++ jobsheet10/Bioskop11.java
@@ -15,13 +15,16 @@ public class Bioskop11 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
+        
+        System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+       
         System.out.println(penonton.length);
         for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+i) + " : " + penonton[i].length);
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
         }
-       for (int i = 0; i < penonton.length; i++) {
-       System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(",", penonton[i]));
-    }
 
     }
 }
