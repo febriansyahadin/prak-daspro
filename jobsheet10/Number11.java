008db2a886bd2b8653f295399870fd9a1a191697
febriansyahadin <febriansyahhoho@gmail.com>
2023-11-01 07:34:27 +0700

    pembaruan

--- jobsheet10/Number11.java
+++ jobsheet10/Number11.java
@@ -1,10 +1,10 @@
+package jobsheet10;
+
 import java.util.Arrays;
-
-
-
-public class Number11 {
+    public class Number11 {
     public static void main(String[] args) {
 
+        int maxColumn = 4;
         int[][] myNumbers = new int[3][];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
@@ -12,6 +12,10 @@ public class Number11 {
 
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + myNumbers[i].length);
         }
 
     }
