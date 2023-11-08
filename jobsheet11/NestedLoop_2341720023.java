45e6aa6e2772305e0b84ab636c0c5545b256a8c5
febriansyahadin <febriansyahhoho@gmail.com>
2023-11-08 10:27:24 +0700

    commit

--- jobsheet11/NestedLoop_2341720023.java
+++ jobsheet11/NestedLoop_2341720023.java
@@ -4,6 +4,7 @@ public class NestedLoop_2341720023 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[][] temps = new double[5][7];
+        double total = 0;
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
@@ -15,10 +16,13 @@ public class NestedLoop_2341720023 {
         }
         for (int i = 0; i < temps [0].length; i++) {
             System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps [0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+            for (double j : temps [i]){
+                System.out.print(j + " ");
+                total += j;
             }
         System.out.println();
+        double rata2 = total / temps [i].length;
+        System.out.println("Rata rata suhu : " + rata2);
         }
     }
 
